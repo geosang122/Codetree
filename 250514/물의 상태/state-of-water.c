70191cf42537f1d05ac64c85ea57d6a7ff4e352a
geosang122 <psh30122@naver.com>
2025-05-14 23:41:44 +0900

    Title: 물의 상태, Time: 7ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 250514/물의 상태/state-of-water.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    // Please write your code here.
+    int t;
+    scanf("%d",&t);
+    if (t<0) printf("ice");
+    else if(t>100) printf("vapor");
+    else printf("water");
+    return 0;
+}
