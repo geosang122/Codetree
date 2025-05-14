8fb6157659cf4e4f0822bb5c9a55cdf3f7c4a166
geosang122 <psh30122@naver.com>
2025-05-14 14:07:04 +0900

    Title: 데이터 교환, Time: 6ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250514/데이터 교환/exchange-data.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    // Please write your code here.
+    int a=5,b=6,c=7,temp;
+    temp=a;
+    a=c;
+    c=b;
+    b=temp;
+    printf("%d\n%d\n%d",a,b,c);
+    return 0;
+}
