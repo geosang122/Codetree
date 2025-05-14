83a8081efce0dbb583e02898d81dd01da966ad6f
geosang122 <psh30122@naver.com>
2025-05-15 02:28:59 +0900

    Title: 두 번의 연산, Time: 7ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250515/두 번의 연산/two-operations.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    // Please write your code here.
+    int a;
+    scanf("%d",&a);
+    if (a%2) a+=3;
+    if (a%3==0) a/=3;
+
+    printf("%d",a);
+    return 0;
+}
