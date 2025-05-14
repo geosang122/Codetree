922e166410f87846468fa894504a821aadc08883
geosang122 <psh30122@naver.com>
2025-05-15 02:50:51 +0900

    Title: 3 또는 5의 배수, Time: 6ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250515/3 또는 5의 배수/multiples-of-3-or-5.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+    // Please write your code here.
+    int a;
+    scanf("%d",&a);
+    a%3==0?printf("YES\n"):printf("NO\n");
+    a%5==0?printf("YES"):printf("NO");
+    return 0;
+}
