201cef8d35ccef706d2fdd6687d4a45dcbd95fb6
geosang122 <psh30122@naver.com>
2025-05-17 02:21:56 +0900

    Title: 코로나 메뉴얼, Time: 6ms, Memory: 0MB, Status: Wrong Answer - Codetree

--- 250517/코로나 메뉴얼/covid-manual.c
+++ 250517/코로나 메뉴얼/covid-manual.c
@@ -6,7 +6,7 @@ int main() {
     int tmp;
     int count=0;
     for (int i=0;i<3;i++){
-        scanf("%c %d",&yn,&tmp);
+        scanf(" %c %d",&yn,&tmp);
         if(yn='Y'&&tmp>=37) count++;
     }
     if (count>=2) printf("E");
