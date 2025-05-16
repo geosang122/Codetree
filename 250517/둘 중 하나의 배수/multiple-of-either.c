f3eeb4e1e63f9d4173db3571f379f9f46f171d8f
geosang122 <psh30122@naver.com>
2025-05-17 00:05:23 +0900

    Title: 둘 중 하나의 배수, Time: 6ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250517/둘 중 하나의 배수/multiple-of-either.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+    // Please write your code here.
+    int A;
+    scanf("%d",&A);
+    if(A%3==0||A%5==0) printf("1");
+    else printf("0");
+    return 0;
+}
