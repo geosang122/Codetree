eee6f7006fccfe9c8d1966f92a20a24242fe6825
geosang122 <psh30122@naver.com>
2025-05-17 20:18:44 +0900

    Title: 별 N개 출력하기, Time: 6ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250517/별 N개 출력하기/print-n-stars.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    // Please write your code here.
+    int n,i=1;
+    scanf("%d",&n);
+    while(i<=n){
+        printf("*\n");
+        i++;
+    }
+    return 0;
+}
