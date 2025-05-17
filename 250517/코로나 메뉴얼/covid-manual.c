3ce2dcb9ade761f92178b77dc37744f9dd4acb26
geosang122 <psh30122@naver.com>
2025-05-17 19:29:39 +0900

    Title: 코로나 메뉴얼, Time: 5ms, Memory: 0MB, Status: Passed - Codetree

--- 250517/코로나 메뉴얼/covid-manual.c
+++ 250517/코로나 메뉴얼/covid-manual.c
@@ -2,15 +2,13 @@
 
 int main() {
     // Please write your code here.
-    char yn1,yn2,yn3;
-    int tmp1,tmp2,tmp3;
+    char yn;
+    int tmp;
     int count=0;
-    scanf(" %c %d",&yn1,&tmp1);
-    scanf(" %c %d",&yn2,&tmp2);
-    scanf(" %c %d",&yn3,&tmp3);
-    if(yn1=='Y'&&tmp1>=37) count++;
-    if(yn2=='Y'&&tmp2>=37) count++;
-    if(yn3=='Y'&&tmp3>=37) count++;
+    for (int i=0;i<3;i++){
+        scanf(" %c %d",&yn,&tmp);
+        if(yn=='Y'&&tmp>=37) count++;
+    }
     if (count>=2) printf("E");
     else printf("N");
 }
