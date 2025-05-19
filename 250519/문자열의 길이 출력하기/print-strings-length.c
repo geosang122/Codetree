59ba7998a257a9cc05075823593139802daba96f
geosang122 <psh30122@naver.com>
2025-05-19 22:19:32 +0900

    Title: 문자열의 길이 출력하기, Time: 6ms, Memory: 0MB, Status: Passed - Codetree

--- 250519/문자열의 길이 출력하기/print-strings-length.c
+++ 250519/문자열의 길이 출력하기/print-strings-length.c
@@ -1,23 +1,10 @@
 #include <stdio.h>
+#include <string.h>
 
 int main() {
     // Please write your code here.
     char s1[101],s2[101];
-    scanf("%s %s",s1,s2);
-    int i=0,j=0;
-    int sum=0;
-    for (i;;i++){
-        if (s1[i]=='\0'){
-            sum+=i;
-            break;
-        }
-    }
-    for (j;;j++){
-        if (s2[j]=='\0'){
-            sum+=j;
-            break;
-        }
-    }
-    printf("%d",sum);
+    scanf("%s %s",s1,s2);    
+    printf("%d",strlen(s1)+strlen(s2));
     return 0;
 }
