68adf434007854503c5c0964e1191d865039aaf3
geosang122 <psh30122@naver.com>
2025-05-20 09:58:34 +0900

    Title: 높은 수에서 낮은 수까지, Time: 6ms, Memory: 0MB, Status: Passed - Codetree

--- 250520/높은 수에서 낮은 수까지/from-high-to-low.c
+++ 250520/높은 수에서 낮은 수까지/from-high-to-low.c
@@ -4,20 +4,16 @@ int main() {
     // Please write your code here.
     int a,b;
     scanf("%d %d",&a,&b);
-    int k=b-a;
-    if (a>b){
-        for (int i=0;i<=k;i++){
+    if (b>a){
+        b=a+b;
+        a=b-a;
+        b=b-a;
+    }
+    int k=a-b;
+    for (int i=0;i<=k;i++){
             printf("%d ",a);
             a--;
-        }
-        
-    }
-    else{
-        for (int i=0;i<=k;i++){
-            printf("%d ",b);
-            b--;
-        }
-        
-    }
+        } 
+       
     return 0;
 }
