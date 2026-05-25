859c9f4ccf2a0c1aace51578be01af80d432c7c0
geosang122 <psh30122@naver.com>
2026-05-25 22:34:34 +0900

    [Passed] 폭탄 해제 (61ms, 16MB)

---
+++ trail2/폭탄 해제/bomb-removal.py
@@ -0,0 +1,8 @@
+unlock_code, wire_color, seconds = input().split()
+seconds = int(seconds)
+
+# Please write your code here.
+
+print(f'code : {unlock_code}')
+print(f'color : {wire_color}')
+print(f'second : {seconds}')
